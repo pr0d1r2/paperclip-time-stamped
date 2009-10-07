225c58fcce345d7f9c1888f6384d5b34567e1ebf
Marcin Nowicki <pr0d1r2@ragnarson.com>
2009-10-07 15:59:14 +0200

    Get rid of annoing warning: multiple values for a block parameter (0 for 1)

--- lib/paperclip-time-stamped.rb
+++ lib/paperclip-time-stamped.rb
@@ -5,24 +5,24 @@ ActiveRecord::Base.class_eval do
 
   def self.has_timestamped_paperclip_attachment(name)
 
-    define_method("#{name}_url") do |style|
-      send(name).url(style)
+    define_method("#{name}_url") do |*style|
+      send(name).url(style.first)
     end
 
-    define_method("#{name}_path") do |style|
-      send(name).path(style)
+    define_method("#{name}_path") do |*style|
+      send(name).path(style.first)
     end
 
-    define_method("#{name}_timestamp") do |style|
-      send("#{name}?", style) ? File.mtime(send("#{name}_path", style)).to_i.to_s : '1000000000'
+    define_method("#{name}_timestamp") do |*style|
+      send("#{name}?", style.first) ? File.mtime(send("#{name}_path", style.first)).to_i.to_s : '1000000000'
     end
 
-    define_method("timestamped_#{name}_url") do |style|
-      [send("#{name}_url", style), '?', send("#{name}_timestamp", style)].join
+    define_method("timestamped_#{name}_url") do |*style|
+      [send("#{name}_url", style.first), '?', send("#{name}_timestamp", style.first)].join
     end
 
-    define_method("#{name}?") do |style|
-      style ? File.file?(send("#{name}_path", style)) : send(name).exists?
+    define_method("#{name}?") do |*style|
+      style.empty? ? send(name).exists? : File.file?(send("#{name}_path", style.first))
     end
 
   end
